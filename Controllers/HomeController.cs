abf335dc45d11918dac1816e7f01c5fb0abf7d4c
Omid Golmakani <OmidGolmakani@outlook.com>
2021-06-24 00:05:01 +0430

    Create Base Controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 namespace CurrencyExchange.Controllers
 {
     [Authorize]
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
