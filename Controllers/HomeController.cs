b57576518d499f2947ceaab02bed2054ee9f9d74
Omid Golmakani <OmidGolmakani@outlook.com>
2021-06-24 00:12:21 +0430

    Remove Default Code From Home Controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,22 +19,5 @@ namespace CurrencyExchange.Controllers
         {
             _logger = logger;
         }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [AllowAnonymous]
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
