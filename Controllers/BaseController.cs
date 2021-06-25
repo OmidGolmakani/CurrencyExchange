a348cbe4d23f628367eb042bcc2675ee3772d95a
Omid Golmakani <OmidGolmakani@outlook.com>
2021-06-25 20:47:42 +0430

    Create Base Controller

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,8 +7,13 @@ using System.Threading.Tasks;
 
 namespace CurrencyExchange.Controllers
 {
-    public class BaseController : Controller
+    public class BaseController<T> : Controller, IController<T> where T : class, new()
     {
+        private readonly IMapper _mapper;
+        public BaseController(IMapper mapper)
+        {
+            this._mapper = mapper;
+        }
         public IActionResult Index()
         {
             return View();
