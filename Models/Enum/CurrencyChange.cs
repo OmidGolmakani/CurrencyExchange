d0872c80df7fc4c02a856d5127b348f8ebe86045
Omid Golmakani <OmidGolmakani@outlook.com>
2021-06-25 21:42:07 +0430

    Create CurrencyChange Validation

---
+++ Models/Enum/CurrencyChange.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyExchange.Models.Enum
+{
+    public class CurrencyChange
+    {
+        internal enum ChangeType
+        {
+            Sale = 1,
+            Buy = 2
+        }
+    }
+}
---
+++ Models/Vakidation/CurrencyChangeValidator.cs
@@ -0,0 +1,25 @@
+﻿using CurrencyExchange.Data;
+using CurrencyExchange.Models.Entity;
+using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CurrencyExchange.Validation
+{
+    public class CurrencyChangeValidator : AbstractValidator<CurrencyChange>
+    {
+        private readonly ApplicationDbContext context;
+        public CurrencyChangeValidator(ApplicationDbContext dbContext)
+        {
+            context = dbContext;
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.CurrencyChangeId).Must(x => EnumHelper.Helper.EnumValidator<Models.Enum.CurrencyChange.ChangeType>(x)).WithMessage("لطفا نوع را مشخص نمایید");
+            RuleFor(x => x.CurrencyPrice).NotNull().NotEmpty().WithMessage("مبلغ ارز اجباری می باشد");
+            RuleFor(x => x.CurrencyId).NotEmpty().NotNull().WithMessage("نوع ارز را مشخص نمایید");
+            RuleFor(x => x.CreateDate).NotEmpty().NotNull().WithMessage("تاریخ اجباری می باشد");
+        }
+
+    }
+}
