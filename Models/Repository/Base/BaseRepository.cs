644921ea46ddff2d8d0d3b831f90cc4fa77c7c9d
Omid Golmakani <OmidGolmakani@outlook.com>
2021-06-25 16:05:39 +0430

    Create Base Repository

---
+++ Models/Repository/Base/BaseRepository.cs
@@ -0,0 +1,64 @@
+﻿using CurrencyExchange.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyExchange.Models.IRepository
+{
+    public abstract class BaseRepository<T> : IBaseRepository<T> where T : class, new()
+    {
+        protected readonly ApplicationDbContext _context;
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().AddRange(entities);
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Where(expression);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().AsQueryable();
+
+        }
+
+        public T GetById(object id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public void Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().RemoveRange(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+
+        public void UpdateRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().UpdateRange(entities);
+        }
+    }
+}
---
+++ Models/Repository/Base/IBaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyExchange.Models.IRepository
+{
+    public interface IBaseRepository<T>
+    {
+        T GetById(object id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> expression);
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Update(T entity);
+        void UpdateRange(IEnumerable<T> entities);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
