b88607ea671cac064739bb2fae88af728f1c9e12
Omid Golmakani <OmidGolmakani@outlook.com>
2021-06-26 00:05:02 +0430

    add TradesValidation

---
+++ Models/Vakidation/TradesValidator.cs
@@ -0,0 +1,29 @@
+﻿using CurrencyExchange.Data;
+using CurrencyExchange.Models.Entity;
+using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CurrencyExchange.Validation
+{
+    public class TradesValidator : AbstractValidator<Trades>
+    {
+        private readonly ApplicationDbContext context;
+        public TradesValidator(ApplicationDbContext dbContext)
+        {
+            context = dbContext;
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.CreateDate).NotEmpty().NotNull().WithMessage("تاریخ سفارش اجباری می باشد");
+            RuleFor(x => x.UserId).NotNull().NotEmpty().WithMessage("لطفا کاربر را انتخاب نمایید");
+            RuleFor(x => x.CurrencyId).NotEmpty().NotNull().WithMessage("نوع ارز اجباری می باشد");
+            RuleFor(x => x.Quantity).NotNull().NotEmpty().WithMessage("تعداد اجباری می باشد");
+            RuleFor(x => x.Quantity).Must(x => x <= 0 ? false : true).WithMessage("تعداد نمیتواند کوچکتر مساوی صفر باشد");
+            RuleFor(x => x.CurrencyPrice).NotNull().NotEmpty().WithMessage("قیمت ارز اجباری می باشد");
+            RuleFor(x => x.InstantPrice).NotNull().Empty().WithMessage("قیمت لحظه ای ارز اجباری می باشد");
+            RuleFor(x => x.Status).NotEmpty().NotNull().WithMessage("وضعیت درخواست اجباری می باشد");
+        }
+
+    }
+}
