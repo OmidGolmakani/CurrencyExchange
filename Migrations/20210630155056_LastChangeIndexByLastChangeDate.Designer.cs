3c4bd1f79f1f0a5460e31d0a42f9da084804de4a
Omid Golmakani <OmidGolmakani@outlook.com>
2021-06-30 20:21:43 +0430

    Add Index For LastChangeDate

--- Migrations/20210630155056_LastChangeIndexByLastChangeDate.Designer.cs
+++ Migrations/20210630155056_LastChangeIndexByLastChangeDate.Designer.cs
@@ -4,14 +4,16 @@ using CurrencyExchange.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CurrencyExchange.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210630155056_LastChangeIndexByLastChangeDate")]
+    partial class LastChangeIndexByLastChangeDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20210630155056_LastChangeIndexByLastChangeDate.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CurrencyExchange.Migrations
+{
+    public partial class LastChangeIndexByLastChangeDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CurrencyChange_LastChangeDate_LastChangeTime",
+                table: "CurrencyChange");
+
+            migrationBuilder.DropColumn(
+                name: "LastChangeTime",
+                table: "CurrencyChange");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CurrencyChange_LastChangeDate",
+                table: "CurrencyChange",
+                column: "LastChangeDate",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CurrencyChange_LastChangeDate",
+                table: "CurrencyChange");
+
+            migrationBuilder.AddColumn<int>(
+                name: "LastChangeTime",
+                table: "CurrencyChange",
+                type: "int",
+                maxLength: 8,
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CurrencyChange_LastChangeDate_LastChangeTime",
+                table: "CurrencyChange",
+                columns: new[] { "LastChangeDate", "LastChangeTime" },
+                unique: true);
+        }
+    }
+}
