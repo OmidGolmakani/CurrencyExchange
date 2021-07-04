d6421b53ac8570a657adb10b53b34f9967371fb4
Omid Golmakani <OmidGolmakani@outlook.com>
2021-07-05 00:34:22 +0430

    change trades table

---
+++ Migrations/20210704194516_AlterTradesChangeBuyDateAndBuyNum.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CurrencyExchange.Migrations
+{
+    public partial class AlterTradesChangeBuyDateAndBuyNum : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BuyNum",
+                table: "Trades",
+                newName: "TradeNum");
+
+            migrationBuilder.RenameColumn(
+                name: "BuyDate",
+                table: "Trades",
+                newName: "TradeDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TradeNum",
+                table: "Trades",
+                newName: "BuyNum");
+
+            migrationBuilder.RenameColumn(
+                name: "TradeDate",
+                table: "Trades",
+                newName: "BuyDate");
+        }
+    }
+}
