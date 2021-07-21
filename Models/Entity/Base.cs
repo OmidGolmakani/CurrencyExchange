e4e376bb0e12208bf1f97cf583f00cbdcfb80523
Omid Golmakani <OmidGolmakani@outlook.com>
2021-07-21 20:46:57 +0430

    fill create or modify date base entity

--- Models/Entity/Base.cs
+++ Models/Entity/Base.cs
@@ -9,10 +9,26 @@ namespace CurrencyExchange.Models.Entity
     {
         public Base()
         {
-            
+            SetDefaultValues();
         }
         public bool Deleted { get; set; }
         public DateTime? CreateDate { get; set; }
         public DateTime? LastModifyDate { get; set; }
+        private void SetDefaultValues()
+        {
+            var Id = this.GetType().GetProperty("Id");
+            if (Id != null)
+            {
+                var value = Id.GetValue(this, null);
+                if (Convert.ToInt64(value) == 0)
+                {
+                    if (CreateDate == null) CreateDate = DateTime.Now;
+                }
+                else
+                {
+                    if (LastModifyDate == null) LastModifyDate = DateTime.Now;
+                }
+            }
+        }
     }
 }
